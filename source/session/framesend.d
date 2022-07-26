182d0b5dcabc85ccdb27e69bf7d1b8ecc50b56ad
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-07-27 01:55:26 +0200

    Only initialize spout if SpoutLibrary is found

--- source/session/framesend.d
+++ source/session/framesend.d
@@ -23,9 +23,11 @@ void insInitFrameSending() {
     version(Windows) {
         auto loadMode = loadSpout2();
         loadSuccessful = loadMode == Spout2Support.spout2;
-        spHandle = spGetSpout();
-        spSetSenderName(spHandle, "Inochi Session");
-        spSetSenderFormat(spHandle, 28); // DXGI 8-bit RGBA
+        if (loadSuccessful) {
+            spHandle = spGetSpout();
+            spSetSenderName(spHandle, "Inochi Session");
+            spSetSenderFormat(spHandle, 28); // DXGI 8-bit RGBA
+        }
     }
 
     if (!loadSuccessful) insLogErr(_("Could not start texture sharing, it will be disabled. Is the library missing?"));
