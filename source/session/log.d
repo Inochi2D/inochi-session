f4f18df395c95aa35565578d098ef57f67080e36
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-07-31 01:52:18 +0200

    Added insLogDebug

--- source/session/log.d
+++ source/session/log.d
@@ -2,29 +2,24 @@ module session.log;
 import std.stdio : writeln;
 import std.format;
 
-void insLogInfo(T...)(string fmt, T args) {
+
+void insLogDebug(T...)(string fmt, T args) {
     version(Windows) {
-        version(InLite) writeln("[INFO] ", fmt.format(args));
-        else debug writeln("[INFO] ", fmt.format(args));
+        version(InLite) writeln("[DEBUG] ", fmt.format(args));
+        else debug writeln("[DEBUG] ", fmt.format(args));
     } else {
-        writeln("[INFO] ", fmt.format(args));
+        writeln("[DEBUG] ", fmt.format(args));
     }
+}
+
+void insLogInfo(T...)(string fmt, T args) {
+    writeln("[INFO] ", fmt.format(args));
 }
 
 void insLogWarn(T...)(string fmt, T args) {
-    version(Windows) {
-        version(InLite) writeln("[WARN] ", fmt.format(args));
-        else debug writeln("[WARN] ", fmt.format(args));
-    } else {
-        writeln("[WARN] ", fmt.format(args));
-    }
+    writeln("[WARN] ", fmt.format(args));
 }
 
 void insLogErr(T...)(string fmt, T args) {
-    version(Windows) {
-        version(InLite) writeln("[ERR ] ", fmt.format(args));
-        else debug writeln("[ERR ] ", fmt.format(args));
-    } else {
-        writeln("[ERR ] ", fmt.format(args));
-    }
+    writeln("[ERR ] ", fmt.format(args));
 }
