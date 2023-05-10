3553bbbf9d04b6f428e70933a1bc15c20450e090
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-05-10 19:52:03 +0200

    Restructure build folders

--- build-aux/windows/version.h
+++ build-aux/windows/version.h
@@ -1,16 +1,16 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_FILEVERSION 1,0,0,0
-#define VER_FILEVERSION_STR "1.0\0"
+#define VER_FILEVERSION 0,8,1,0
+#define VER_FILEVERSION_STR "0.8.1\0"
 
-#define VER_PRODUCTVERSION          1,0,0,0
-#define VER_PRODUCTVERSION_STR      "1.0\0"
+#define VER_PRODUCTVERSION          0,8,1,0
+#define VER_PRODUCTVERSION_STR      "0.8.1\0"
 
 #define VER_COMPANYNAME_STR         "Inochi2D Project"
 #define VER_FILEDESCRIPTION_STR     "Inochi Session"
 #define VER_INTERNALNAME_STR        "inochi-session"
-#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2022 Inochi2D Project"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright © 2022-2023 Inochi2D Project"
 #define VER_ORIGINALFILENAME_STR    "inochi-session.exe"
 #define VER_PRODUCTNAME_STR         "Inochi Session"
 
